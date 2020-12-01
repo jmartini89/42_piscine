0718e60d5db11b5c2b68065ce817d918be138e44
Jacopo <jacopomartini89@gmail.com>
2020-12-01 22:10:28 +0100

    C00k

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -10,16 +10,17 @@ void	ft_putchar(char a)
 
 void	ft_putnbr(int nb)
 {
-	if(nb == -2147483648)//DAFARE
+	if(nb == -2147483648)
 	{
-		write -2147483648
+		write(1, "-2147483648", 11);
+		return;
 	}
 	else if (nb < 0)
 	{
 		ft_putchar('-');
 		ft_putnbr(nb * -1);
 	}
-	else if(nb < 10)//send to write!
+	else if(nb < 10)
 	{
 		ft_putchar(nb + 48);
 	}
@@ -32,5 +33,5 @@ void	ft_putnbr(int nb)
 
 int	main(void)
 {
-	ft_putnbr(-2147483648);
+	ft_putnbr(2147483647);
 }
