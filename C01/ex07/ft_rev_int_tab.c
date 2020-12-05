25c8c559cb28b25a5c9419c47529eb4e60c766a0
Jacopo Martini <jmartini@student.42roma.it>
2020-12-05 20:26:47 +0100

    C0ddue

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -9,6 +9,7 @@
 /*   Updated: 2020/12/05 14:25:22 by jmartini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 void	ft_rev_int_tab(int *tab, int size)
 {
@@ -26,5 +27,24 @@ void	ft_rev_int_tab(int *tab, int size)
 		*(tab + rvr) = tmp;
 		frw++;
 		rvr--;
+	}
+}
+
+int	main(void)
+{
+	int tab[4];
+	int size;
+	int i;
+
+	tab[0] = -10;
+	tab[1] = 34;
+	tab[2] = 56;
+	tab[3] = 78;
+	size = 4;
+	ft_rev_int_tab(&tab[0], size);
+	while (i != size)
+	{
+		printf("%d,", tab[i]);
+		i++;
 	}
 }
--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -6,16 +6,45 @@
 /*   By: jmartini <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 15:46:40 by jmartini          #+#    #+#             */
-/*   Updated: 2020/12/05 15:47:30 by jmartini         ###   ########.fr       */
+/*   Updated: 2020/12/05 20:25:55 by jmartini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src);
+#include <stdio.h>
+#include <unistd.h>
+
+char	ft_strcpy(char *dest, char *src)//SAREBBE FUNZIONE PUNTATA!
 {
-	WUT
+	int index;
+	int test;//TEST
+
+	index = 0;
+	test = 1;//TEST
+	while (*src >= '\0')//SEGFAULT HERE!!!
+	{
+		dest[index] = src[index];
+		index++;
+		test--;
+	}
+	return (0);
 }
 
 int		main(void)
 {
-	MH
+	char dest[12] = "destination";
+	char src[4]; 
+	src[0] = 'X';
+	src[1] = 'Y';
+	src[2] = 'Z';
+	int iterator = 0;
+
+	ft_strcpy(dest, src);
+	write(1, &dest, 11);
+	/*while (dest[iterator] != '\0')
+	{
+		printf("%c", dest);
+		iterator++;
+	}*/
 }
+
+//segfault : accessing 'not allowed/wrong' memory address
