07e877cbe83a5fe5505d949d770f25a69f72acbf
Nikopol <jacopomartini89@gmail.com>
2020-12-20 11:46:07 +0100

    caprarola

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -10,10 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int		ft_atoi(char *str)
 {
-	//
+	int i;
+	int sign;
+	int number;
+
+	i = 0;
+	sign = 0;
+	number = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == 32)//isspace
+		i++;
+	while (str[i] == '-' || str[i] == '+')//checksign
+	{
+		if (str[i] == '-')
+			sign--;
+		if (str[i] == '+')
+			sign++;
+		i++;
+	}
+	if (sign >= 0)
+		sign = 1;
+	else if (sign < 0)
+		sign = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = (number * 10) + ((str[i] - '0'));
+		i++;
+	}
+	return (number * sign);
 }
+
+int 	main()
+{
+	char test[] = "        --1234 ab567";
+	printf("EMULAZIONE: %d\n", ft_atoi(test));
+}
+
 //3 while non innestati
-//
-//
---
+++ C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int		ft_putnbr_base(int nbr, char *base)
+{
+	asd
+}
+
+int		main()
+{
+	int n = 42;
+	
+	char dec[] = "0123456789";
+	char bin[] = "01";
+	char exa[] = "0123456789ABCDEF";
+	char oct[] = "poneyvif";
+	ft_putnbr_base(n, dec);
+}
