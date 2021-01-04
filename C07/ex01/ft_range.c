dfbff66bc01dc0e469018179089b0973f9cabc73
Nikopol <jacopomartini89@gmail.com>
2021-01-04 23:29:34 +0100

    nullmaybe

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -9,12 +9,10 @@ int		*ft_range(int min, int max)
 
 	it = 0;
 	alloc = max - min;
-	str = malloc(sizeof(int) * alloc);
+	if (alloc < 0)
+		alloc = 0;
+	str = malloc(sizeof(int) * alloc + 1);
 	while (min < max)
-	printf("%ld\n", sizeof(str));
-	str = malloc(sizeof(100));
-	printf("%ld\n", sizeof(str));
-	while (str[it])
 	{
 		str[it] = min;
 		it++;
@@ -25,24 +23,17 @@ int		*ft_range(int min, int max)
 
 int		main(void)
 {
-	int min;
-	int max;
+	int min = 29;
+	int max = 30;
+	int it = 0;
 	int *out;
-	int it;
-	int a[1];
-	int it;
 
-	a[0] = '\0';
-	min = 31;
-	max = 30;
 	it = 0;
 	out = ft_range(min, max);
-	printf("%d\n", a[0]);
-	/*while (out[it])
+	while (out[it])
 	{
 		printf("%d\n", out[it]);
 		it++;
-	}*/
-	ft_range(min, max);
+	}
 	return (0);
 }
---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int		ft_ultimate_range(int **range, int min, int max);
+
+int		main(void)
+{
+	int min = 0;
+	int max = 0;
+}
